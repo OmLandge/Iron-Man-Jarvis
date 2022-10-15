d421351df41383b95aec5563df8b46b62bdd4cf8
Om Landge <94532488+OmLandge@users.noreply.github.com>
2022-10-15 19:48:27 +0530

    Update config.py

--- JARVIS-master/Jarvis/config/config.py
+++ JARVIS-master/Jarvis/config/config.py
@@ -1,4 +1,4 @@
-email = "omlandge0000@gmail.com"
-email_password = "om@@11om"
-wolframalpha_id = "295VAW-574375JWXE"
-weather_api_key = "88a4e8304dbdcd6177814ed45c421ca8"
+email = "Your email"
+email_password = "your email password"
+wolframalpha_id = "your wolframalpha id"
+weather_api_key = "your weather api key"
